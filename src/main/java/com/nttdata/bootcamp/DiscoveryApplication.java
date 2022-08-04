deb002f967b26ce3fd664a9ee145c3009b0349cb
jbenavidesg27 <jbenavidesg27@gmail.com>
2022-08-04 17:41:32 -0500

    Checklyst completado

--- src/main/java/com/nttdata/bootcamp/DiscoveryApplication.java
+++ src/main/java/com/nttdata/bootcamp/DiscoveryApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Application Eureka server.
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DiscoveryApplication.class, args);
+  }
 
 }
